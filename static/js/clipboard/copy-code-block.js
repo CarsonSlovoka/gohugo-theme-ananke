3ef7b5963a5e548a0b80bb9e9b1a462e1ee0a680
Carson <jackparadise520a@gmail.com>
2021-02-13 23:00:20 +0800

    [copy-code-block] 使用fontawesome來取代文字 Use the icon(provided by font-awesome) instead of the text. <i class="far fa-copy"></i>

--- static/js/clipboard/copy-code-block.js
+++ static/js/clipboard/copy-code-block.js
@@ -2,11 +2,15 @@
 
 function addCopyButtons(clipboard) {
   document.querySelectorAll('pre > code').forEach(function (codeBlock) {
+
     const button = document.createElement('button');
     button.className = 'copy-code-button';  // 為了配合CSS而已
     button.type = 'button';
-    button.innerText = 'Copy';
+    // button.innerText = 'Copy';
+    button.title = 'Copy';
 
+    const icon_copy = document.createElement('i');  // fontawesome: <i class="far fa-copy"></i>
+    icon_copy.className = "far fa-copy"
 
     button.addEventListener('click', function () {
       clipboard.writeText(codeBlock.innerText).then(function () {
@@ -14,10 +18,12 @@ function addCopyButtons(clipboard) {
            leaving the button in a focused state. */
         button.blur();
 
+        const org_innerText = button.innerText
         button.innerText = 'Copied!';
 
         setTimeout(function () {
-          button.innerText = 'Copy';
+          button.innerText = org_innerText;
+          button.appendChild(icon_copy)  // 因為我們改變了innerText，所以要重新再加入
         }, 1000);
       }, function (error) {
         button.innerText = 'Error';
@@ -31,6 +37,7 @@ function addCopyButtons(clipboard) {
     } else {
       pre.parentNode.insertBefore(button, pre);
     }
+    button.appendChild(icon_copy)
   });
 }
 
